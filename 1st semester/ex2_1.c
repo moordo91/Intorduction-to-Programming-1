f0f3673334433dd7f2af73c5be80acb52cb94420
Dale, Han <82254758+moordo91@users.noreply.github.com>
2022-04-01 11:56:12 +0900

    Rename Introduction to Programming(1)/ex2_1.c to 1st semester/ex2_1.c

---
+++ 1st semester/ex2_1.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+void main() {
+	int a = 123;
+	printf("현재 a값에 5를 더한 결과=%d\n", a + 5);
+	printf("현재 a값에서 100을 뺀 결과=%d\n", a - 100);
+	printf("현재 a값에 2를 곱한 결과=%d\n", a * 2);
+	printf("현재 a값에 5를 나눈 결과=%d\n", a / 5);
+}
